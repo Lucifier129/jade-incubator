f7a5b37ffd602c4c2540867d2e7f129ccf47e12f
Jade Gu <guyingjie129@163.com>
2021-05-04 09:46:11 +0800

    refactor(react-x): implement recoil-like api

--- packages/react-x/src/index.ts
+++ packages/react-x/src/index.ts
@@ -86,12 +86,94 @@ const sprsh3: Tasks<string, number> = (key) => {
   return null
 }
 
+type Atom<T = unknown> = {
+  type: 'Atom'
+  key: string
+  default: T
+}
+
+const atom = <T>(options: Omit<Atom<T>, 'type'>): Atom<T> => {
+  return {
+    ...options,
+    type: 'Atom',
+  }
+}
+
+type Selector = {
+  type: 'Selector'
+  key: string
+  get: ({ get }: { get: Fetch<Selector | Atom, number> }) => number
+}
+
+const selector = (options: Omit<Selector, 'type'>): Selector => {
+  return {
+    ...options,
+    type: 'Selector',
+  }
+}
+
+type Rule = Atom | Selector
+
+type Rules = Rule[]
+
+const A1 = atom({
+  key: 'A1',
+  default: 10,
+})
+
+const A2 = atom({
+  key: 'A2',
+  default: 20,
+})
+
+const B1 = selector({
+  key: 'B1',
+  get: ({ get }) => {
+    let a1 = get(A1)
+    let a2 = get(A2)
+    return a1 + a2
+  },
+})
+
+const B2 = selector({
+  key: 'B2',
+  get: ({ get }) => {
+    let b1 = get(B1)
+    return b1 * 2
+  },
+})
+
+const busySprsh = (rules: Rules, input: Selector | Atom, store: Store<string, number>) => {
+  let tasks: Tasks<string, number> = (key) => {
+    let rule = rules.find((item) => item.key === key)
+
+    if (!rule) {
+      return null
+    }
+
+    if (rule.type === 'Atom') {
+      let value = rule.default as number
+      return () => value
+    }
+
+    let task = rule.get
+
+    return (fetch) => {
+      return task({ get: ({ key }) => fetch(key) })
+    }
+  }
+
+  return busy(tasks, input.key, store)
+}
+
 let store: Store<string, number> = new Map([
   ['A1', 10],
   ['A2', 20],
 ])
 
-busy(sprsh1, 'B2', store)
+// busy(sprsh1, 'B2', store)
+
+busySprsh([A1, A2, B1, B2], B2, store)
 
 console.log('B1', store.get('B1'))
 console.log('B2', store.get('B2'))
